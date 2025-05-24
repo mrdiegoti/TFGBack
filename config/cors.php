d638c751000b3d9172846a0faa3aaf68078abe66
mrdiegoti <daw05.2024@gmail.com>
2025-05-24 23:15:06 +0200

    h

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'https://hoopsfever.vercel.app'],
+    'allowed_origins' => ['https://hoopsfever.vercel.app', 'http://localhost:4200'],
 
     'allowed_origins_patterns' => [],
 
