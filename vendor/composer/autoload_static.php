0a0c5657eb259ce62a522a4211106bc190c6cad7
mrdiegoti <daw05.2024@gmail.com>
2025-05-24 23:53:02 +0200

    vendor

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -367,8 +367,8 @@ class ComposerStaticInitefa7b14ad79d708682982c55d7a835dc
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
